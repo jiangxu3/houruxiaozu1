57282db576678684bfb1bd21e70b022e1190d8e6
Aamir shayan <aamirshayanshaikh@gmail.com>
2022-10-29 14:05:34 +0500

    spring-boot-best-practices

---
+++ src/main/java/com/javakar/springbootbestpractices/course/CourseDto.java
@@ -0,0 +1,27 @@
+package com.javakar.springbootbestpractices.course;
+
+import lombok.*;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CourseDto {
+
+    private long id;
+    @NotNull @NotEmpty
+    private String courseName;
+    @Min(value = 1, message = "The number of bags must be greater than 0")
+    private int courseDuration;
+
+    public Course toCourse(){
+        return new Course()
+                .setCourseName(courseName)
+                .setCourseDuration(courseDuration);
+    }
+}
---
+++ src/main/java/com/javakar/springbootbestpractices/student/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.javakar.springbootbestpractices.student;
+
+import com.javakar.springbootbestpractices.authentication.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<User, Long> {
+
+}
