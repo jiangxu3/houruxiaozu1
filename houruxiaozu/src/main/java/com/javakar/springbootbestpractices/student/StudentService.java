6ee3c05323a33b1c4bc4bf3b38359f4114c16a4c
ltyq123 <1433372804@qq.com>
2025-05-13 19:59:57 +0800

    Add files via upload

---
+++ houruxiaozu/src/main/java/com/javakar/springbootbestpractices/student/StudentService.java
@@ -0,0 +1,20 @@
+package com.javakar.springbootbestpractices.student;
+
+import com.javakar.springbootbestpractices.authentication.user.User;
+import com.javakar.springbootbestpractices.authentication.user.UserDto;
+
+import java.util.List;
+
+public interface StudentService {
+
+    UserDto createStudent(UserDto userDto);
+    List<UserDto> createStudents(List<UserDto> userDtoList);
+    UserDto findStudentById(Long studentId);
+    List<UserDto> findAllStudents();
+    UserDto updateStudent(UserDto userDto);
+    List<UserDto> updateStudents(List<UserDto> userDtoList);
+    void deleteStudent(UserDto userDto);
+    void deleteStudents(List<UserDto> userDtoList);
+    void findStudentsByCourse(User user);
+
+}
